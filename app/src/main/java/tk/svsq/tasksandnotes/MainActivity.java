7e92ce0589f16a883e827db36c872506a0595100
Slaventij Bilokur <svsq@outlook.com>
2017-05-29 16:06:41 +0300

    * Inserted Test image to Splash Screen * Created Splash Screen

--- app/src/main/java/tk/svsq/tasksandnotes/MainActivity.java
+++ app/src/main/java/tk/svsq/tasksandnotes/MainActivity.java
@@ -1,13 +1,30 @@
 package tk.svsq.tasksandnotes;
 
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    FragmentManager fragmentManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fragmentManager = getSupportFragmentManager();
+
+        runSplash();
+    }
+
+    public void runSplash()   {
+
+        SplashFragment splashFragment = new SplashFragment();
+        fragmentManager.beginTransaction()
+                .replace(R.id.containter, splashFragment)
+                .addToBackStack(null)
+                .commit();
+
     }
 }
