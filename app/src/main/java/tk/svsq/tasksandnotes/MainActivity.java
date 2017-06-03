6c6535c5b06a306abf295bd702c6e22892b765c1
Slaventij Bilokur <svsq@outlook.com>
2017-06-03 15:55:22 +0300

    * fixes * new settings menu icon

--- app/src/main/java/tk/svsq/tasksandnotes/MainActivity.java
+++ app/src/main/java/tk/svsq/tasksandnotes/MainActivity.java
@@ -80,9 +80,9 @@ public class MainActivity extends AppCompatActivity {
         TabAdapter tabAdapter = new TabAdapter(fragmentManager, 2);
 
         viewPager.setAdapter(tabAdapter);
-        viewPager.addOnAdapterChangeListener((ViewPager.OnAdapterChangeListener) new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
 
-        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
                 viewPager.setCurrentItem(tab.getPosition());
