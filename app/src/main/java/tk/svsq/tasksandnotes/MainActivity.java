2eeb173953107adda0b350d1d2e2c01c83754bd2
Slaventij Bilokur <svsq@outlook.com>
2017-06-05 17:12:51 +0300

    * added floating action button * fixed dialogs.s

--- app/src/main/java/tk/svsq/tasksandnotes/MainActivity.java
+++ app/src/main/java/tk/svsq/tasksandnotes/MainActivity.java
@@ -1,18 +1,23 @@
 package tk.svsq.tasksandnotes;
 
+import android.app.DialogFragment;
 import android.app.FragmentManager;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 import tk.svsq.tasksandnotes.adapters.TabAdapter;
+import tk.svsq.tasksandnotes.dialog.AddingTaskDialogFragment;
 import tk.svsq.tasksandnotes.fragments.SplashFragment;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AddingTaskDialogFragment.AddingTaskListener {
 
     FragmentManager fragmentManager;
 
@@ -98,5 +103,24 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogFragment addingTaskDialogFragment = new AddingTaskDialogFragment();
+                addingTaskDialogFragment.show(fragmentManager, "AddingTaskDialogFragment");
+            }
+        });
+    }
+
+    @Override
+    public void onTaskAdded() {
+        Toast.makeText(this, "Task added", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onTaskAddingCancel() {
+        Toast.makeText(this, "Cancel adding task", Toast.LENGTH_LONG).show();
     }
 }
--- app/src/main/java/tk/svsq/tasksandnotes/dialog/AddingTaskDialogFragment.java
+++ app/src/main/java/tk/svsq/tasksandnotes/dialog/AddingTaskDialogFragment.java
@@ -3,11 +3,15 @@ package tk.svsq.tasksandnotes.dialog;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.TextInputLayout;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TimePicker;
@@ -23,6 +27,24 @@ import tk.svsq.tasksandnotes.fragments.Utils;
 
 public class AddingTaskDialogFragment extends DialogFragment {
 
+    private AddingTaskListener addingTaskListener;
+
+    public interface AddingTaskListener {
+        void onTaskAdded();
+
+        void onTaskAddingCancel();
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        try {
+            addingTaskListener = (AddingTaskListener) context;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(context.toString() + "must implement AddingTaskListener");
+        }
+    }
+
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
 
@@ -34,8 +56,8 @@ public class AddingTaskDialogFragment extends DialogFragment {
 
         View container = inflater.inflate(R.layout.dialog_task, null);
 
-        TextInputLayout tilTitle = (TextInputLayout) container.findViewById(R.id.tilDialogTaskTitle);
-        EditText etTitle = tilTitle.getEditText();
+        final TextInputLayout tilTitle = (TextInputLayout) container.findViewById(R.id.tilDialogTaskTitle);
+        final EditText etTitle = tilTitle.getEditText();
 
         TextInputLayout tilDate = (TextInputLayout) container.findViewById(R.id.tilDialogTaskDate);
         final EditText etDate = tilDate.getEditText();
@@ -77,7 +99,7 @@ public class AddingTaskDialogFragment extends DialogFragment {
         etTime.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(etTime.length() == 0)  {
+                if (etTime.length() == 0) {
                     etTime.setText(" ");
                 }
 
@@ -95,9 +117,62 @@ public class AddingTaskDialogFragment extends DialogFragment {
                         etTime.setText(null);
                     }
                 };
+                timePickerFragment.show(getFragmentManager(), "TimePickerFragment");
             }
         });
 
-        return super.onCreateDialog(savedInstanceState);
+        builder.setPositiveButton(R.string.dialog_ok, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                addingTaskListener.onTaskAdded();
+                dialog.dismiss();
+            }
+        });
+
+        builder.setNegativeButton(R.string.dialog_cancel, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                addingTaskListener.onTaskAddingCancel();
+                dialog.cancel();
+            }
+        });
+
+        AlertDialog alertDialog = builder.create();
+        alertDialog.setOnShowListener(new DialogInterface.OnShowListener() {
+
+            @Override
+            public void onShow(DialogInterface dialog) {
+                final Button positiveButton = ((AlertDialog) dialog).getButton(DialogInterface.BUTTON_POSITIVE);
+                if (etTitle.length() == 0) {
+                    positiveButton.setEnabled(false);
+                    tilTitle.setError(getResources().getString(R.string.dialog_error_empty_title));
+                }
+
+                etTitle.addTextChangedListener(new TextWatcher() {
+                    @Override
+                    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+                    }
+
+                    @Override
+                    public void onTextChanged(CharSequence s, int start, int before, int count) {
+                        if (s.length() == 0) {
+                            positiveButton.setEnabled(false);
+                            tilTitle.setError(getResources().getString(R.string.dialog_error_empty_title));
+                        } else {
+                            positiveButton.setEnabled(true);
+                            tilTitle.setErrorEnabled(false);
+                        }
+                    }
+
+                    @Override
+                    public void afterTextChanged(Editable s) {
+
+                    }
+                });
+            }
+        });
+
+        return alertDialog;
     }
 }
