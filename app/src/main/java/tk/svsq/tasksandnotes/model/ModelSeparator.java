65ddfd861f9ea26a06700927aa4538029c302fef
Slaventij Bilokur <svsq@outlook.com>
2017-12-02 13:46:36 +0200

    [TN10 WIP] Start working for Day10 task - adding separators and ability  for edit tasks.

---
+++ app/src/main/java/tk/svsq/tasksandnotes/model/ModelSeparator.java
@@ -0,0 +1,15 @@
+package tk.svsq.tasksandnotes.model;
+
+import tk.svsq.tasksandnotes.R;
+
+/**
+ * Created by svsq on 12/2/2017.
+ */
+
+public class ModelSeparator {
+    public static final int TYPE_OVERDUE = R.string.separator_overdue;
+    public static final int TYPE_TODAY = R.string.separator_today;
+    public static final int TYPE_TOMORROW = R.string.separator_tomorrow;
+    public static final int TYPE_FUTURE = R.string.separator_future;
+
+}
